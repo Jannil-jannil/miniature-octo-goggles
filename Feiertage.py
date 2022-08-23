37ba0b9083d26744ea3d4f1d8903ec6cd3186a19
jannil-jannil <ja.schaerli@hotmail.com>
2022-08-23 08:59:39 +0200

    Kommentierung

--- Feiertage.py
+++ Feiertage.py
@@ -3,10 +3,11 @@
 import datetime
 import math
 
-
+#plus minus rechnung für die Tage um Ostern
 def osterrechnung(tage):
 	return osterdatum + datetime.timedelta(days=tage)
 
+#Daten von String in Objekt umwandeln
 def datumkonvert(datum):
 	return datetime.datetime.strptime(datum, "%m/%d/%Y")
 
@@ -16,6 +17,7 @@ zeit = datetime.datetime.now().time()
 minzeit= datetime.time(7,55)
 maxzeit= datetime.time(17)
 
+#Osterrechnung
 a = jahr % 19
 b = jahr % 4
 c = jahr % 7
@@ -30,7 +32,7 @@ e = (2 * b + 4 * c + 6 * d + N) % 7
 ostersonntag = (22 + d + e)
 
 monat = 3
-
+#Wenn Ostern im April ist und nicht im März
 if(ostersonntag > 31):
 	ostersonntag = ostersonntag % 31
 	monat = 4
@@ -38,12 +40,12 @@ if(ostersonntag > 31):
 ostern = (f"{monat}/{ostersonntag}/{jahr}")
 
 osterdatum = datumkonvert(ostern)
-
+#Berechnen der Feiertage um Ostern
 karfreitag = osterrechnung(-2) 
 ostermontag = osterrechnung(1)
 auffahrt = osterrechnung(39)
 pfingstmontag = osterrechnung(50)
-
+#Die festen Feiertage
 neujahr = datumkonvert(f"01/01/{jahr}")
 berchtoldstag = datumkonvert(f"01/02/{jahr}")
 nationalfeiertag = datumkonvert(f"08/01/{jahr}")
@@ -51,9 +53,11 @@ weihnachten = datumkonvert(f"12/25/{jahr}")
 stephanstag = datumkonvert(f"12/26/{jahr}")
 #testtag = datumkonvert(f"08/22/{jahr}")
 
+#Das heutige Datum in die richtige formatierung setzen
 heuteSTR = datetime.date.today().strftime("%m/%d/%Y")
 heute = datumkonvert(heuteSTR)
 
+#Array mit allen Feiertagen
 feiertage = [
 karfreitag,
 ostermontag,
@@ -67,6 +71,6 @@ stephanstag,
 #testtag
 ]
 
-
+#Wenn kein feiertag und in Arbeitszeit führ das main Skript aus 
 if(heute not in feiertage and minzeit <= zeit <= maxzeit):
 	exec(open("Mail.py").read())
--- Mail.py
+++ Mail.py
@@ -5,16 +5,20 @@ from aioimaplib import aioimaplib
 import requests
 
 
-
+#Mailüberprüfung wird unendlich ausgeführt bis der Process von Cronjobs geschlossen wird
 async def check_mailbox(host, user, password):
 	while True:
+		#Login zum Exchange Server
 		imap_client = aioimaplib.IMAP4_SSL(host=host)
 		await imap_client.wait_hello_from_server()
 
 		await imap_client.login(user, password)
 		await imap_client.select()
+		#Mailbox durchsuchen nach dem 'unSeen' Attribut
 		search, lines = await imap_client.search('unSeen', charset='US-ASCII')
 		mailcount = (len(lines[0]))
+
+		#Wenn ein Mail nicht gelesen wurde schick ein http request an den smartsocket
 		if mailcount> 0:
 			requests.get('http://10.18.20.158/control?cmd=GPIO,12,1')
 		else:
